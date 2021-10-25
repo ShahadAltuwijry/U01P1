1c9e40490bf7a59c1f7e1b2cae06d933a44ef2d6
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-10-25 12:06:01 +0300

    completed project content

--- Home.js
+++ Home.js
@@ -1,80 +1,87 @@
 const villains = [
   {
     name: "Ursula (The Sea Witch)",
-    imgUrl: "./ursula.jpg",
+    imgUrl: "./media/ursula.jpg",
+    fullImg: "./media/full Ursula.jpg",
     miniDes:
       "In tone, and by nature, Ursula was very dark with a wicked sense of humor. ",
-    banner: "./ursula banner.png",
+    banner: "./media/ursula banner.png",
     quoteHead:
       "“Triton's daughter will be mine and then I'll make him writhe. I'll see him wriggle like a worm on a hook!”",
     quoteCrid: "- Ursula, 'The Little Mermaid'.",
     description:
       "Ursula (also known as the Sea Witch) is the main antagonist of Disney's 1989 animated feature film The Little Mermaid. She is a villainous Cecaelia (half-woman, half-octopus) who strikes deals with unfortunate merfolk with the promise of making their dreams come true. Ursula's contracts, however, are covertly designed to advance her own ambitions and cause general misery. In the past, Ursula was a resident of King Triton's palace, before being banished and exiled by order of the king. Scorned, she has since vowed to exact vengeance by harnessing the power of the king's trident and installing herself as ruler of all the ocean. Honestly I don't see Ursula as a Villain in this movie and that's because Ariel read the contract and signed it, agreeing to all the condetions so why is it Ursula's fault that she's following the laws and coming to take what's hers? Some people will just agree with the main character of the movie just because he is, but if we think logically Ursula wasn't at fault but a lot of people might not agree with me on that. ",
-    // photos:{"./ursula1.webp" "./ursula2.webp" } ,
+    // photos:{"./full Ursula.jpg" } ,
   },
   {
     name: "Scar",
-    imgUrl: "./scar.jpg",
+    imgUrl: "./media/scar.jpg",
+    fullImg: "./media/full scar.png",
     miniDes:
       "Scar is heavily sarcastic, frequently replying to attempts at conversation with sarcasm and subtle insults.",
-    banner: "./ursula banner.png",
-    quoteHead:
-      "“Triton's daughter will be mine and then I'll make him writhe. I'll see him wriggle like a worm on a hook!”",
-    quoteCrid: "- Ursula, 'The Little Mermaid'.",
+    banner: "./media/scar banner.jpg",
+    quoteHead: "“ Long live the king. ”",
+    quoteCrid:
+      "- Scar showing his true nature to his brother Mufasa before throwing him off the side of the gorge to the stampede below to his death and his most infamous quote.",
     description:
-      "Ursula (also known as the Sea Witch) is the main antagonist of Disney's 1989 animated feature film The Little Mermaid. She is a villainous Cecaelia (half-woman, half-octopus) who strikes deals with unfortunate merfolk with the promise of making their dreams come true. Ursula's contracts, however, are covertly designed to advance her own ambitions and cause general misery. In the past, Ursula was a resident of King Triton's palace, before being banished and exiled by order of the king. Scorned, she has since vowed to exact vengeance by harnessing the power of the king's trident and installing herself as ruler of all the ocean. Honestly I don't see Ursula as a Villain in this movie and that's because Ariel read the contract and signed it, agreeing to all the condetions so why is it Ursula's fault that she's following the laws and coming to take what's hers? Some people will just agree with the main character of the movie just because he is, but if we think logically Ursula wasn't at fault but a lot of people might not agree with me on that. ",
+      "Askari, better known as Scar, is the main antagonist of Disney's The Lion King franchise. He is Mufasa's younger brother and Simba's uncle and arch-nemesis. The former leader of the Lion Guard, his desire to rule Pride Rock drives him to enact a regicidal plot to kill both his brother and nephew to leave himself the sole royal in line for the throne as the 'true' ruler. In the franchise itself, Scar serves as the main antagonist of Disney's thirty-second full-length animated feature film The Lion King, the posthumous overarching antagonist of its 1998 sequel The Lion King II: Simba's Pride, the overarching antagonist of both the 2004 prequel/parallel film The Lion King 1½ and the animated television series The Lion Guard (serving as the posthumous overarching antagonist of Season 1 and most of Season 3 and the main antagonist of Season 2 and the Season 3 premiere 'Battle for the Pride Lands'.), and a cameo character in The Lion King's Timon and Pumbaa.",
     // photos:{"./ursula1.webp" "./ursula2.webp"
   },
   {
     name: "Maleficent",
-    imgUrl: "./maleficent.jpg",
+    imgUrl: "./media/maleficent.jpg",
+    fullImg: "./media/full maleficent.jpg",
     miniDes:
       "Maleficent represents pure evil. She is ruthless, dark, devious, and will do whatever it takes to achieve her evil goals.",
-    banner: "./ursula banner.png",
+    banner: "./media/maleficent banner.jfif",
     quoteHead:
-      "“Triton's daughter will be mine and then I'll make him writhe. I'll see him wriggle like a worm on a hook!”",
-    quoteCrid: "- Ursula, 'The Little Mermaid'.",
+      "“ Now, shall you deal with ME, o Prince, and all the powers of HELL! (maniacal laugh) ”",
+    quoteCrid:
+      "- Maleficent's last words before her battle with Prince Phillip and her death.",
     description:
-      "Ursula (also known as the Sea Witch) is the main antagonist of Disney's 1989 animated feature film The Little Mermaid. She is a villainous Cecaelia (half-woman, half-octopus) who strikes deals with unfortunate merfolk with the promise of making their dreams come true. Ursula's contracts, however, are covertly designed to advance her own ambitions and cause general misery. In the past, Ursula was a resident of King Triton's palace, before being banished and exiled by order of the king. Scorned, she has since vowed to exact vengeance by harnessing the power of the king's trident and installing herself as ruler of all the ocean. Honestly I don't see Ursula as a Villain in this movie and that's because Ariel read the contract and signed it, agreeing to all the condetions so why is it Ursula's fault that she's following the laws and coming to take what's hers? Some people will just agree with the main character of the movie just because he is, but if we think logically Ursula wasn't at fault but a lot of people might not agree with me on that. ",
+      "Maleficent is the main antagonist of Disney's 16th full-length animated feature film Sleeping Beauty (which is based on the 1697 classic fairytale of the same name by the late Charles Perrault and the late Peter Tchaikovsky), a supporting antagonist in Mickey's House of Villains, one of the main antagonists of the Kingdom Hearts franchise, and the titular main protagonist of the live-action films Maleficent and Maleficent: Mistress of Evil. In Sleeping Beauty, she is the arch-nemesis of the Three Good Fairies, Princess Aurora, and Prince Phillip. She is an evil witch who takes offense at not being invited to the christening of Princess Aurora and attempts to get revenge on King Stefan and his people by cursing Queen Leah and Stefan's beloved daughter, Princess Aurora.",
     // photos:{"./ursula1.webp" "./ursula2.webp"
   },
   {
     name: "The Joker",
-    imgUrl: "./joker.jpg",
+    imgUrl: "./media/joker.jpg",
+    fullImg: "./media/full joker.jpg",
     miniDes:
       "He has a malevolent, sadistic sense of humor and psychopathic tendency of murdering whoever he wants.",
-    banner: "./ursula banner.png",
+    banner: "./media/joker banner.jpg",
     quoteHead:
       "“Triton's daughter will be mine and then I'll make him writhe. I'll see him wriggle like a worm on a hook!”",
-    quoteCrid: "- Ursula, 'The Little Mermaid'.",
+    quoteCrid: "-  The Joker's most famous quote.	",
     description:
-      "Ursula (also known as the Sea Witch) is the main antagonist of Disney's 1989 animated feature film The Little Mermaid. She is a villainous Cecaelia (half-woman, half-octopus) who strikes deals with unfortunate merfolk with the promise of making their dreams come true. Ursula's contracts, however, are covertly designed to advance her own ambitions and cause general misery. In the past, Ursula was a resident of King Triton's palace, before being banished and exiled by order of the king. Scorned, she has since vowed to exact vengeance by harnessing the power of the king's trident and installing herself as ruler of all the ocean. Honestly I don't see Ursula as a Villain in this movie and that's because Ariel read the contract and signed it, agreeing to all the condetions so why is it Ursula's fault that she's following the laws and coming to take what's hers? Some people will just agree with the main character of the movie just because he is, but if we think logically Ursula wasn't at fault but a lot of people might not agree with me on that. ",
+      "The Joker is one of the main antagonists of DC Comics, most often pitted against his archenemy Batman, Superman, and occasionally the Justice League as a whole as a part of the Legion of Doom. In the comics, the Joker is portrayed as a master criminal whose characterization has varied. The original and currently dominant image is of a highly intelligent homicidal maniac with a twisted, sadistic sense of humor. His most prominent origin story is that he fell into a vat of chemicals, which bleached his skin white, turned his hair green and turned his lips bright red, giving him the appearance of a clown. His crazed grin, psychotic, clown-like appearance and sadistic practical jokes all make the Joker one of the most diabolical villains of DC Comics and of all time. ",
     // photos:{"./ursula1.webp" "./ursula2.webp"
   },
   {
     name: "Harley Quinn",
-    imgUrl: "./harley.png",
+    imgUrl: "./media/harley.png",
+    fullImg: "./media/full harley2.png",
     miniDes:
       "Bubbly, energetic, and charming, she often hides her demented mind behind a childish exterio. Blindly loyal to the Clown Prince of Crime",
-    banner: "./ursula banner.png",
+    banner: "./media/harley banner.jpg",
     quoteHead:
-      "“Triton's daughter will be mine and then I'll make him writhe. I'll see him wriggle like a worm on a hook!”",
-    quoteCrid: "- Ursula, 'The Little Mermaid'.",
+      "“ My love for my Joker was stronger than their madhouse walls. ”",
+    quoteCrid: "- Harley Quinn expressing her love of the Joker.	",
     description:
-      "Ursula (also known as the Sea Witch) is the main antagonist of Disney's 1989 animated feature film The Little Mermaid. She is a villainous Cecaelia (half-woman, half-octopus) who strikes deals with unfortunate merfolk with the promise of making their dreams come true. Ursula's contracts, however, are covertly designed to advance her own ambitions and cause general misery. In the past, Ursula was a resident of King Triton's palace, before being banished and exiled by order of the king. Scorned, she has since vowed to exact vengeance by harnessing the power of the king's trident and installing herself as ruler of all the ocean. Honestly I don't see Ursula as a Villain in this movie and that's because Ariel read the contract and signed it, agreeing to all the condetions so why is it Ursula's fault that she's following the laws and coming to take what's hers? Some people will just agree with the main character of the movie just because he is, but if we think logically Ursula wasn't at fault but a lot of people might not agree with me on that. ",
+      "Dr. Harleen Quinzel, known more commonly as Harley Quinn is a major antagonist of the DC Animated Universe. She was originally the Joker's accomplice and on and off girlfriend, but eventually struck out on her own. She is often aided in her crimes by her best friend, Poison Ivy. She is the archenemy of Batgirl. She is a major antagonist in Batman: The Animated Series and its sequel The New Batman Adventures and one of the four main protagonists of the Gotham Girls spinoff web series. ",
     // photos:{"./ursula1.webp" "./ursula2.webp"
   },
   {
     name: "Dr. Facilier",
-    imgUrl: "./dr. facilier.jpg",
+    imgUrl: "./media/dr. facilier.jpg",
+    fullImg: "./media/full dr.jpg",
     miniDes:
       "Facilier is a scheming bokor, while a polite, charismatic showman, he is also cruel, remorseless, and sadistic.",
-    banner: "./ursula banner.png",
+    banner: "./media/dr banner.jpg",
     quoteHead:
-      "“Triton's daughter will be mine and then I'll make him writhe. I'll see him wriggle like a worm on a hook!”",
-    quoteCrid: "- Ursula, 'The Little Mermaid'.",
+      "“ Gentlemen, enchanté. A tip of the hat from Dr. Facilier. How y'all doing? ”",
+    quoteCrid: "- Dr. Facilier first meeting Naveen and Lawrence.	",
     description:
-      "Ursula (also known as the Sea Witch) is the main antagonist of Disney's 1989 animated feature film The Little Mermaid. She is a villainous Cecaelia (half-woman, half-octopus) who strikes deals with unfortunate merfolk with the promise of making their dreams come true. Ursula's contracts, however, are covertly designed to advance her own ambitions and cause general misery. In the past, Ursula was a resident of King Triton's palace, before being banished and exiled by order of the king. Scorned, she has since vowed to exact vengeance by harnessing the power of the king's trident and installing herself as ruler of all the ocean. Honestly I don't see Ursula as a Villain in this movie and that's because Ariel read the contract and signed it, agreeing to all the condetions so why is it Ursula's fault that she's following the laws and coming to take what's hers? Some people will just agree with the main character of the movie just because he is, but if we think logically Ursula wasn't at fault but a lot of people might not agree with me on that. ",
+      "Dr. Facilier, also known as 'The Shadow Man', is the main antagonist of Disney's fourty-ninth full-length animated feature film The Princess and the Frog, which is based on the 2002 children's novel The Frog Princess by E.D. Baker and the German fairytale The Frog Prince by the Brothers Grimm. He is an evil witch doctor/bokor and Lawrence's new employer who plans to rule New Orleans (and eventually, the entire world) with help from his 'friends on the other side'. Since his inception, Facilier has reached critical acclaim and major popularity with fans, making him one of Disney's most popular villains, in addition, to quickly becoming a primary member of the Disney Villains franchise. He is also Tiana and Naveen's arch-nemesis and the one responsible for them being transformed into frogs. ",
     // photos:{"./ursula1.webp" "./ursula2.webp"
   },
 ];
--- desc.js
+++ desc.js
@@ -1,7 +1,6 @@
 const item = JSON.parse(localStorage.getItem("item"));
 
-$(".desc").append(`
-
+$(".wrapper").append(`
             <div class="banner">
             <img src="${item.banner}" id="banner" >
             <h1 class="quoteHead">${item.quoteHead}</h1>
@@ -12,15 +11,18 @@ $(".desc").append(`
 
         <div class="content">
             <div class="imgDiv">
-                <img id="vilImg" class="vilImg" src="${item.imgUrl}">
+                <img id="vilImg" class="vilImg" src="${item.fullImg}">
             </div>
 
             <div class="desContent">
+                <h3 class="h3Head">Details and Backstory</h3>
                 <p class="description" id="description">
                 ${item.description}
                 </p>
             </div>
         </div>
+        <h2>Photo Gallery</h2>
+
     </div>
 `);
 //^ didn't add the photo Gallery yet
